4bca0841675483370fb22b430fe3fa557172af50
andreas.koslowski <andreas.koslowski@xing.com>
2022-04-08 01:50:37 +0200

    Add DiagnosticsEngine.DiagnosticsHandler to add warnings to Xcode

--- Sources/PeckerKit/Reporters/XcodeReporter.swift
+++ Sources/PeckerKit/Reporters/XcodeReporter.swift
@@ -4,17 +4,24 @@ import TSCBasic
 import TSCUtility
 
 public struct XcodeReporter: Reporter {
+    struct Location: DiagnosticLocation {
+        let description: String
+    }
     
     public func report(_ configuration: Configuration, sources: [SourceDetail]) {
         let diagnosticEngine = makeDiagnosticEngine()
         for source in sources {
-            diagnosticEngine.emit(warning: "Pecker: \(source.name) was never used; consider removing it; \(source.location)")
+            diagnosticEngine.emit(warning: "Pecker: '\(source.name)' was never used; consider removing it", location: Location(description: source.location.description))
         }
     }
 }
 
 /// Makes and returns a new configured diagnostic engine.
 private func makeDiagnosticEngine() -> DiagnosticsEngine {
-  let engine = DiagnosticsEngine()
-  return engine
+    let handler: DiagnosticsEngine.DiagnosticsHandler = { diagnostic in
+        // </path/to/file.swift>:<line>:<column>: <warning>: <message>
+        let message = "\(diagnostic.location): warning: \(diagnostic.localizedDescription)"
+        print(message)
+    }
+    return DiagnosticsEngine(handlers: [handler])
 }
